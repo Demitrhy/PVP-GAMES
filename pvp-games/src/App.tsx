b9261629cc1654e3e79ae6f36dbec4f2a9d40a53
Gustavo Teles <gustavp_cordeiro27@outlook.com>
2025-09-11 15:37:40 -0300

    Nova tela de Desconto

--- pvp-games/src/App.tsx
+++ pvp-games/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import GamePrincipal from './Pages/GamePrincipal';
 import GameCarrosel from './Pages/GameCarrosel';
+import GameList from './Pages/GameDesconto';
 
 
 const App: React.FC = () => {
   return (
     <div style={{ backgroundColor: "black", minHeight: "100vh" }}>
       <GamePrincipal />
+      <GameList/>
       <GameCarrosel />
     </div>
   );
